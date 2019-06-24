6db621b6cb1d52e4645b92e5daa3e748ef4e7d5e
Hensikt <42713950+Hensikt@users.noreply.github.com>
2019-06-24 20:28:42 +0200

    Add files via upload

--- NewThema/index.php
+++ NewThema/index.php
@@ -19,40 +19,17 @@ get_header();
                         <?php the_content() ?>
 
                     </article>
-                    <div class="postmetaData">
-                        Geplaasts door:
-                        <?php the_author(); ?>
-                        <br>
-                        op:
-                        <?php the_date(); ?>
-                        <br>
-                        in de catogorie:
-                        <?php the_category(); ?>
-                    </div>
                     <!-- einde container -->
                 <?php endwhile;
             else :
                 echo "<p>Er zijn geen berichten gevonden</p>";
             endif;
             // Einde wordpress loop
-            comments_template("", true)
             ?>
         </div>
         <!--    De aside    -->
         <aside>
             <?php dynamic_sidebar('widget_aside '); ?>
-            <h4>Vorig bericht</h4>
-            <?php
-            $previous_post = get_previous_post();
-            if (!empty($previous_post)): ?>
-                <a href="<?php echo esc_url(get_permalink($previous_post->ID)); ?>"><?php echo esc_attr($previous_post->post_title); ?></a>
-            <?php endif; ?>
-            <h4>Volgende bericht</h4>
-            <?php
-            $next_post = get_next_post();
-            if (!empty($next_post)): ?>
-                <a href="<?php echo esc_url(get_permalink($next_post->ID)); ?>"><?php echo esc_attr($next_post->post_title); ?></a>
-            <?php endif; ?>
         </aside>
     </main>
 </div>
